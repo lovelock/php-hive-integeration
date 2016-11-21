ce2149aefab5df5f412a2ac3d14a3d64e8c03c4d
lovelock <frostwong@gmail.com>
2016-11-21 18:22:57 +0800

    rm unused files

--- example.php
+++ example.php
@@ -1,23 +1,23 @@
 <?php
 
-  // Load this lib
-  require_once __DIR__ . '/ThriftSQL.phar';
+// Load this lib
+require_once __DIR__ . '/vendor/autoload.php';
 
-  // Try out a Hive query
-  $hive = new \ThriftSQL\Hive( 'hive.host.local' );
-  $hiveTables = $hive
+// Try out a Hive query
+$hive = new \ThriftSQL\Hive( 'hive.host.local' );
+$hiveTables = $hive
     ->setSasl( false ) // To turn SASL auth off, on by default
     ->connect()
     ->queryAndFetchAll( 'SHOW TABLES' );
-  print_r( $hiveTables );
+print_r( $hiveTables );
 
-  // Try out an Impala query
-  $impala = new \ThriftSQL\Impala( 'impala.host.local' );
-  $impalaTables = $impala
+// Try out an Impala query
+$impala = new \ThriftSQL\Impala( 'impala.host.local' );
+$impalaTables = $impala
     ->connect()
     ->queryAndFetchAll( 'SHOW TABLES' );
-  print_r( $impalaTables );
+print_r( $impalaTables );
 
-  // Don't forget to clear the client and close socket.
-  $hive->disconnect();
-  $impala->disconnect();
+// Don't forget to clear the client and close socket.
+$hive->disconnect();
+$impala->disconnect();
